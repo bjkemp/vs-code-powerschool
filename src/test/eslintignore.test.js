e2a841bad9e07b45a29c0a03d5e96ad22eb273f2
Benjamin Kemp <bjkemp@gmail.com>
2023-03-15 15:47:38 -0400

    Fixed injection, need to fix lint highlighting

--- src/test/eslintignore.test.js
+++ src/test/eslintignore.test.js
@@ -1,6 +1,21 @@
+
 const data = [
     ~[tlist_sql;
     SELECT 100 things
     FROM dual
     ]~(things)[/tlist_sql]
     ];
+
+~[tlist_sql;SELECT 100 things
+    FROM dual
+    ]~(things)[/tlist_sql]
+
+~[tlist_sql;
+    SELECT * FROM FOO;]~(foo)[/tlist_sql]]
+
+
+~[tlist_sql;
+    SELECT * FROM foo
+]~(foo)[/tlist_sql]
+
+~[tlist_sql;][/tlist_sql]
