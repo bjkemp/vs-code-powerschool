f3ea3488279de81913608239cf5bbed4d457e409
= <=>
2023-03-15 21:51:23 -0400

    Add validateSQL

--- src/extension.ts
+++ src/extension.ts
@@ -6,49 +6,6 @@ const sqlDiagnostics = vscode.languages.createDiagnosticCollection('sql');
 // Define the regex to match tlist_sql tags and extract the SQL code
 const regex = /~\[tlist_sql;([\s\S]*?)\]\s*(.*?)\s*\[\/tlist_sql\]/gm;
 
-function activate(context: vscode.ExtensionContext) {
-  // Register a TextDocumentContentProvider for the "tlist" scheme
-  const provider = new TListContentProvider();
-  context.subscriptions.push(
-    vscode.workspace.registerTextDocumentContentProvider('tlist', provider)
-  );
-
-  // Register a document change listener to check for SQL errors in the tlist_sql tag
-  vscode.workspace.onDidChangeTextDocument((e) => {
-		console.log(e.document.uri.scheme);
-    if (e.document.uri.scheme === 'tlist') {
-      const text = e.document.getText();
-      let match: RegExpExecArray | null;
-      while ((match = regex.exec(text))) {
-        const sql = match[1];
-        const code = match[2];
-				console.log(sql);
-				console.log(code);
-        // Validate the SQL code using an existing SQL linter or parser library
-        const errors = validateSQL(sql, code);
-        // Convert the errors to diagnostics and add them to the diagnostic collection
-        const diagnostics = errors.map((err) => {
-          if (match) {
-            const start = new vscode.Position(match.index + err.line, err.column);
-            const end = new vscode.Position(match.index + err.line, code.split('\n')[err.line].length);
-            const range = new vscode.Range(start, end);
-            return new vscode.Diagnostic(range, err.message, vscode.DiagnosticSeverity.Error);
-          } else {
-            return undefined;
-          }
-        });
-        sqlDiagnostics.set(e.document.uri, diagnostics.filter(diag => diag !== undefined) as vscode.Diagnostic[]);
-      }
-    }
-  });
-}
-
-function validateSQL(sql: string, code: string): { line: number; column: number; message: string }[] {
-  // Use an existing SQL linter or parser library to validate the SQL code
-  // Return an array of error objects with properties `line`, `column`, and `message`
-  return [];
-}
-
 class TListContentProvider implements vscode.TextDocumentContentProvider {
   provideTextDocumentContent(uri: vscode.Uri): vscode.ProviderResult<string> {
     // Fetch the contents of the TList file and return them as a string
@@ -56,7 +13,71 @@ class TListContentProvider implements vscode.TextDocumentContentProvider {
   }
 }
 
-export function deactivate() {
-  // Clear the SQL diagnostics when the extension is deactivated
-  sqlDiagnostics.clear();
+async function validateSQL(sql: string): Promise<vscode.Diagnostic[]> {
+  const diagnostics: vscode.Diagnostic[] = [];
+
+  // Create a diagnostic collection for our custom SQL language
+  const sqlDiagnostics = vscode.languages.createDiagnosticCollection('tlist_sql');
+
+  // Create a new SQL document to hold the SQL code to be validated
+  const doc = await vscode.workspace.openTextDocument({ language: 'sql', content: sql });
+
+  // Retrieve the diagnostics for the SQL document
+  const newDiagnostics = await vscode.languages.getDiagnostics(doc.uri);
+
+  // Add the new diagnostics to the diagnostic collection
+  sqlDiagnostics.set(doc.uri, newDiagnostics);
+
+  // Get the diagnostics for all open SQL documents
+  const allDiagnostics = await vscode.languages.getDiagnostics(doc.uri);
+
+  // Add the diagnostics for all open SQL documents to the diagnostics array
+  allDiagnostics.forEach((diagnostic) => {
+    diagnostics.push(diagnostic);
+  });
+
+  return diagnostics;
 }
+
+
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  // Register a TextDocumentContentProvider for the "tlist" scheme
+  const provider = new TListContentProvider();
+  context.subscriptions.push(
+    vscode.workspace.registerTextDocumentContentProvider('tlist_sql', provider)
+  );
+
+
+  // Register a document change listener to check for SQL errors in the tlist_sql tag
+  vscode.workspace.onDidChangeTextDocument((e) => {
+    const text = e.document.getText();
+    let match: RegExpExecArray | null;
+    while ((match = regex.exec(text))) {
+      const sql = match[1];
+      const result = match[2];
+      console.log(sql);
+      console.log(result);
+      // Validate the SQL code using an existing SQL linter or parser library
+      const errors = validateSQL(sql);
+      // Convert the errors to diagnostics and add them to the diagnostic collection
+      const diagnostics = errors.map((err) => {
+        if (match) {
+          const start = new vscode.Position(match.index + err.line, err.column);
+          const end = new vscode.Position(match.index + err.line, result.split('\n')[err.line].length);
+          const range = new vscode.Range(start, end);
+          return new vscode.Diagnostic(range, err.message, vscode.DiagnosticSeverity.Error);
+        } else {
+          return undefined;
+        }
+      });
+      sqlDiagnostics.set(e.document.uri, diagnostics.filter(diag => diag !== undefined) as vscode.Diagnostic[]);
+    }
+  });
+
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
