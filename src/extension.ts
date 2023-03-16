75572784350695996671995f3a39226ee2dc71c4
= <=>
2023-03-15 20:45:54 -0400

    Adding SQL Diagnostics

--- src/extension.ts
+++ src/extension.ts
@@ -1,85 +1,61 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-enum TokenType {
-	tlist_sql = 1
+// Define a diagnostic collection to hold our SQL errors
+const sqlDiagnostics = vscode.languages.createDiagnosticCollection('sql');
+
+// Define the regex to match tlist_sql tags and extract the SQL code
+const regex = /~\[tlist_sql;([\s\S]*?)\]\s*(.*?)\s*\[\/tlist_sql\]/gm;
+
+function activate(context: vscode.ExtensionContext) {
+  // Register a TextDocumentContentProvider for the "tlist" scheme
+  const provider = new TListContentProvider();
+  context.subscriptions.push(
+    vscode.workspace.registerTextDocumentContentProvider('tlist', provider)
+  );
+
+  // Register a document change listener to check for SQL errors in the tlist_sql tag
+  vscode.workspace.onDidChangeTextDocument((e) => {
+    if (e.document.uri.scheme === 'tlist') {
+      const text = e.document.getText();
+      let match: RegExpExecArray | null;
+      while ((match = regex.exec(text))) {
+        const sql = match[1];
+        const code = match[2];
+				console.log(sql);
+				console.log(code);
+        // Validate the SQL code using an existing SQL linter or parser library
+        const errors = validateSQL(sql, code);
+        // Convert the errors to diagnostics and add them to the diagnostic collection
+        const diagnostics = errors.map((err) => {
+          if (match) {
+            const start = new vscode.Position(match.index + err.line, err.column);
+            const end = new vscode.Position(match.index + err.line, code.split('\n')[err.line].length);
+            const range = new vscode.Range(start, end);
+            return new vscode.Diagnostic(range, err.message, vscode.DiagnosticSeverity.Error);
+          } else {
+            return undefined;
+          }
+        });
+        sqlDiagnostics.set(e.document.uri, diagnostics.filter(diag => diag !== undefined) as vscode.Diagnostic[]);
+      }
+    }
+  });
 }
 
-const selector: vscode.DocumentSelector = [
-	{scheme: 'file', language: 'json'},
-	{scheme: 'file', language: 'html'},
-	{scheme: 'file', language: 'javascript'},
-	{scheme: 'file', language: 'text'},
-]
-
-const provider: vscode.DocumentSemanticTokensProvider = {
-	provideDocumentSemanticTokens(
-  	document: vscode.TextDocument
-	): vscode.ProviderResult<vscode.SemanticTokens> {
-		const builder = new vscode.SemanticTokensBuilder();
-		console.log(`Tokenizing document: ${document.uri}`);
-
-		const regex = /~\[tlist_sql;([\s\S]*?)\]\s*(.*?)\s*\[\/tlist_sql\]/gm;
-		let match: RegExpExecArray | null;
-		while ((match = regex.exec(document.getText())) !== null) {
-			console.log(`Found tlist_sql token: ${match[0]}`);
-			const start = document.positionAt(match.index);
-			const end = document.positionAt(match.index + match[0].length);
-
-			builder.push(
-				start.line,
-				start.character,
-				match[1].trim().split(/\s+/g).length, // token length
-				TokenType.tlist_sql
-			);
-
-			builder.push(
-				end.line,
-				end.character,
-				0, // token length
-				0
-			);
-		}
-
-		console.log(`Tokenizing completed for document: ${document.uri}`);
-		return builder.build();
-	}
-};
-
-const legend = new vscode.SemanticTokensLegend(
-  ['tlist_sql'],
-  []
-);
-
-const providerRegistration = vscode.languages.registerDocumentSemanticTokensProvider(
-  selector,
-  provider,
-  legend
-);
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	context.subscriptions.push(providerRegistration);
-	console.log('[TLIST_SQL]Provider registration added to context.subscriptions:', providerRegistration);
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vs-code-powerschool" is now active!');
-
-	// // The command has been defined in the package.json file
-	// // Now provide the implementation of the command with registerCommand
-	// // The commandId parameter must match the command field in package.json
-	// let disposable = vscode.commands.registerCommand('vs-code-powerschool.helloWorld', () => {
-	// 	// The code you place here will be executed every time your command is executed
-	// 	// Display a message box to the user
-	// 	vscode.window.showInformationMessage('Hello World from VS Code - PowerSchool!');
-	// });
-
-	// context.subscriptions.push(disposable);
+function validateSQL(sql: string, code: string): { line: number; column: number; message: string }[] {
+  // Use an existing SQL linter or parser library to validate the SQL code
+  // Return an array of error objects with properties `line`, `column`, and `message`
+  return [];
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+class TListContentProvider implements vscode.TextDocumentContentProvider {
+  provideTextDocumentContent(uri: vscode.Uri): vscode.ProviderResult<string> {
+    // Fetch the contents of the TList file and return them as a string
+    return "";
+  }
+}
+
+export function deactivate() {
+  // Clear the SQL diagnostics when the extension is deactivated
+  sqlDiagnostics.clear();
+}
